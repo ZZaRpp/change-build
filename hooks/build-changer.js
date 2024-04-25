b28087823d7756027cbf63e6f06159af28dfd249
ZZaRpp <neozarp@gmail.com>
2024-04-25 10:55:53 +0200

    Update build-changer.js

--- hooks/build-changer.js
+++ hooks/build-changer.js
@@ -3,7 +3,9 @@ const utils = require("./ar-config");
 module.exports = function (context) {
     const confs = utils.getConfigs();
     //const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
+    
     utils.logAppFolders(context.opts.projectRoot);
+    utils.logAppFolders(context.opts.projectRoot + confs.androidRootPath);
     utils.logAppFolders(context.opts.projectRoot + confs.androidAppPath);
     utils.logAppFolders(context.opts.projectRoot + confs.androidMainPath);
     utils.logAppFolders(context.opts.projectRoot + confs.androidPath);
