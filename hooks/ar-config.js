e4d1fb4d12e816836a5f6c4c19480206cb6dd72d
ZZaRpp <neozarp@gmail.com>
2024-04-25 18:18:40 +0200

    Update ar-config.js

--- hooks/ar-config.js
+++ hooks/ar-config.js
@@ -59,7 +59,7 @@ function changeProjectProperties() {
     let path = "platforms/android/project.properties";
     logFile(path);
     let strToFind = "android.library.reference.2=app";
-    let replaceByStr = "android.library.reference.2=app" + os.EOL + "android.library.reference.3=unityLibrary" + os.EOL;
+    let replaceByStr = "android.library.reference.2=app" + os.EOL + "android.library.reference.3=unityLibrary" + os.EOL + "unityStreamingAssets=.unity3d, google-services-desktop.json, google-services.json, GoogleService-Info.plist"+ os.EOL;
     changeFileContent(path,strToFind,replaceByStr);
     //Log the changed file
     logFile(path);
